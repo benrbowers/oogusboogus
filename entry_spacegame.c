3a9e704c1cb57d564daa1f65ce2b7ad3ad4dd192
benrbowers <benjambowers@gmail.com>
2024-09-21 16:23:38 -0400

    Show tile selector when hovering entity rect

--- entry_spacegame.c
+++ entry_spacegame.c
@@ -3,23 +3,19 @@
 #define SCALE 0.1875	// 240 / 1280
 #define TILE_SIZE 12
 
-bool almost_equals(float a, float b, float epsilon);
-bool animate_f32_to_target(
-	float* value,
-	float target,
-	float delta_t,
-	float rate
-);
-void animate_v2_to_target(
-	Vector2* value,
-	Vector2 target,
-	float delta_t,
-	float rate
-);
-
-Vector2 screen_to_world();
-int world_to_tile_unit(float world_unit);
-Vector2i world_to_tile_pos(Vector2 world_pos);
+typedef enum SpriteId {
+	SPRITE_player,
+	SPRITE_energy_crystal,
+	SPRITE_crystal_drop,
+	SPRITE_berry_stalk,
+	SPRITE_berry_drop,
+	SPRITE_tile_selector_tl,
+	SPRITE_tile_selector_tr,
+	SPRITE_tile_selector_bl,
+	SPRITE_tile_selector_br,
+	SPRITE_MAX
+} SpriteId;
+Gfx_Image* sprites[SPRITE_MAX];
 
 typedef enum EntityType {
 	ENT_nil = 0,
@@ -47,77 +43,46 @@ typedef struct Entity {
 typedef struct World {
 	Entity entities[MAX_ENTITIES];
 } World;
-World* world = NULL;
 
-/* Setup Functions */
+bool almost_equals(float a, float b, float epsilon);
+bool animate_f32_to_target(
+	float* value,
+	float target,
+	float delta_t,
+	float rate
+);
+void animate_v2_to_target(
+	Vector2* value,
+	Vector2 target,
+	float delta_t,
+	float rate
+);
 
-Entity* entity_create() {
-	Entity* entity_found = NULL;
+Vector2 screen_to_world();
+int world_to_tile_unit(float world_unit);
+Vector2i world_to_tile_pos(Vector2 world_pos);
+float tile_to_world_unit(int tile_unit);
+Vector2 tile_to_world_pos(Vector2i tile_pos);
 
-	for (int i = 0; i < MAX_ENTITIES; i++) {
-		Entity* existing_entity = &world->entities[i];
-		if (!existing_entity->is_valid) {
-			entity_found = existing_entity;
-			break;
-		}
-	}
+void draw(Entity* ent);
+void draw_tile_selector(Entity* ent, float64 elapsed_time);
 
-	assert(entity_found, "No more free entities.");
-
-	return entity_found;
-}
-
-void entity_destroy(Entity* ent) {
-	memset(ent, 0, sizeof(Entity));
-}
-
-Range2f entity_get_rect(Entity* ent) {
-	return range2f_bottom_center(
-		ent->pos, v2_add(ent->pos, ent->size)
-	);
-}
-
-void draw(Entity* ent) {
-	draw_image(
-		ent->sprite,
-		v2(ent->pos.x - (ent->size.x / 2), ent->pos.y),
-		ent->size,
-		ent->color
-	);
-}
-
-void setup_player(Entity* player, Gfx_Image* sprite) {
-	player->is_valid = true;
-	player->type = ENT_player;
-	player->sprite = sprite;
-	player->color = COLOR_WHITE;
-	player->size = v2(sprite->width, sprite->height);
-	player->pos = v2(0, 0);
-	player->rect = entity_get_rect(player);
-}
-
+Entity* entity_create();
+void entity_destroy(Entity* ent);
+Range2f entity_get_rect(Entity* ent);
+void setup_player(Entity* player);
 void setup_entity(
 	Entity* ent,
 	EntityType type,
 	Gfx_Image* sprite
-) {
-	float32 width = WINDOW_WIDTH * SCALE;
-	float32 height = WINDOW_HEIGHT * SCALE;
-	float32 x = get_random_float32_in_range(
-		width / -2, (width / 2) - sprite->width
-	);
-	float32 y = get_random_float32_in_range(
-		height / -2, (height / 2) - sprite->height
-	);
+);
 
-	ent->is_valid = true;
-	ent->type = type;
-	ent->sprite = sprite;
-	ent->color = COLOR_WHITE;
-	ent->size = v2(sprite->width, sprite->height);
-	ent->pos = v2(x, y);
-	ent->rect = entity_get_rect(ent);
-}
+void load_sprites(Allocator allocator);
+
+World* world = NULL;
+
+Vector2i tile_size = (Vector2i){TILE_SIZE, TILE_SIZE};
+Vector2 tile_sizef = (Vector2){TILE_SIZE, TILE_SIZE};
 
 int entry(int argc, char** argv) {
 	// This is how we (optionally) configure the window.
@@ -133,65 +98,42 @@ int entry(int argc, char** argv) {
 
 	world = alloc(allocator, sizeof(World));
 
-	Gfx_Image* player_spr = load_image_from_disk(
-		STR("assets/astronaut.png"), allocator
-	);
-	assert(player_spr, "Failed to load player sprite.");
-	Gfx_Image* energy_crystal_spr = load_image_from_disk(
-		STR("assets/energy-crystal.png"), allocator
-	);
-	assert(
-		energy_crystal_spr,
-		"Failed to load energy_crystal sprite."
-	);
-	Gfx_Image* crystal_drop_spr = load_image_from_disk(
-		STR("assets/crystal-drop.png"), allocator
-	);
-	assert(
-		crystal_drop_spr, "Failed to load crystal_drop sprite."
-	);
-	Gfx_Image* berry_stalk_spr = load_image_from_disk(
-		STR("assets/berry-stalk.png"), allocator
-	);
-	assert(
-		berry_stalk_spr, "Failed to load berry_stalk sprite."
-	);
-	Gfx_Image* berry_drop_spr = load_image_from_disk(
-		STR("assets/berry-drop.png"), allocator
-	);
-	assert(
-		berry_drop_spr, "Failed to load berry_drop sprite."
-	);
+	load_sprites(allocator);
 
 	printf(
 		"Player sprite size: %d x %d\n",
-		player_spr->width,
-		player_spr->height
+		sprites[SPRITE_player]->width,
+		sprites[SPRITE_player]->height
 	);
 
 	Entity* berry_stalk = entity_create();
 	setup_entity(
-		berry_stalk, ENT_berry_stalk, berry_stalk_spr
+		berry_stalk,
+		ENT_berry_stalk,
+		sprites[SPRITE_berry_stalk]
 	);
 
 	for (int i = 0; i < 3; i++) {
 		Entity* crystal = entity_create();
-		setup_entity(crystal, ENT_crystal, energy_crystal_spr);
+		setup_entity(
+			crystal, ENT_crystal, sprites[SPRITE_energy_crystal]
+		);
 	}
 
 	Entity* player = entity_create();
-	setup_player(player, player_spr);
+	setup_player(player);
 
-	Vector2i tile_size = v2i(TILE_SIZE, TILE_SIZE);
-	Vector2 tile_sizef = v2(TILE_SIZE, TILE_SIZE);
 	Vector4 tile_color = v4(1, 1, 1, 0.2);
 
 	Vector2 camera_pos = v2(0, 0);
 
-	s32 frame_count = 0;
+	int frame_count = 0;
 	float64 frame_timer = 0.0;
 
 	float64 last_time = 0.0;
+
+	Vector2 mouse_pos = v2(0, 0);
+	bool user_is_hovering_entity = false;
 	float64 hover_start = 0.0;
 
 	int hovered_entity_index = -1;
@@ -249,7 +191,7 @@ int entry(int argc, char** argv) {
 			player->rect = entity_get_rect(player);
 		}
 
-		Vector2i player_tile_pos
+		const Vector2i player_tile_pos
 			= world_to_tile_pos(player->pos);
 		const int tile_radius_x
 			= window.width * SCALE / 2 / TILE_SIZE + 5;
@@ -277,40 +219,40 @@ int entry(int argc, char** argv) {
 			}
 		}
 
-		Vector2 mouse_pos = screen_to_world();
+		mouse_pos = screen_to_world();
+		user_is_hovering_entity = false;
 
 		// Check for hover
 		for (int i = MAX_ENTITIES - 1; i >= 0; i--) {
 			Entity entity = world->entities[i];
 
 			if (entity.is_valid) {
+				if (entity.type == ENT_player)
+					continue;
+
 				if (range2f_contains(entity.rect, mouse_pos)) {
 					if (hovered_entity_index != i) {
 						hovered_entity_index = i;
 						hover_start = now;
 					}
 
+					user_is_hovering_entity = true;
 					break;
-				}
-			}
-
-			if (i == 0) {
-				if (hovered_entity_index >= 0) {
-					hovered_entity_index = -1;
 				}
 			}
 		}
 
+		if (!user_is_hovering_entity) {
+			if (hovered_entity_index >= 0) {
+				hovered_entity_index = -1;
+			}
+		}
+
 		if (hovered_entity_index >= 0) {
-			Entity hovered_entity
-				= world->entities[hovered_entity_index];
-			draw_rect(
-				hovered_entity.rect.min,
-				hovered_entity.size,
-				v4(
-					1, 1, 1, fabs(sin(5 * (now - hover_start))) * 0.2
-				)
-			);
+			Entity* hovered_entity
+				= &world->entities[hovered_entity_index];
+
+			draw_tile_selector(hovered_entity, now - hover_start);
 		}
 
 		for (int i = 0; i < MAX_ENTITIES; i++) {
@@ -413,9 +355,202 @@ Vector2i world_to_tile_pos(Vector2 world_pos) {
 		world_to_tile_unit(world_pos.y)
 	);
 }
-Vector2 tile_pos_to_world(Vector2i tile_pos) {
+Vector2 tile_to_world_pos(Vector2i tile_pos) {
 	return v2(
 		tile_to_world_unit(tile_pos.x),
 		tile_to_world_unit(tile_pos.y)
+	);
+}
+
+/* Draw Functions */
+
+void draw(Entity* ent) {
+	if (!ent->is_valid)
+		return;
+
+	draw_image(
+		ent->sprite,
+		v2(ent->pos.x - (ent->size.x / 2), ent->pos.y),
+		ent->size,
+		ent->color
+	);
+}
+
+void draw_tile_selector(Entity* ent, float64 elapsed_time) {
+	if (!ent->is_valid)
+		return;
+	if (elapsed_time < 0.0)
+		return;
+
+	float offset = 6 * fabs(sin(6.5 * elapsed_time)) + 2.0;
+
+	Vector2 pos = tile_to_world_pos(ent->tile_pos);
+	pos = v2_sub(pos, v2(offset / 2.0f, offset / 2.0f));
+	Vector2 size
+		= v2_mul(v2i_to_v2(ent->tile_size), tile_sizef);
+	size = v2_add(size, v2(offset, offset));
+
+	float spr_width = sprites[SPRITE_tile_selector_tl]->width;
+	Vector2 spr_size = v2(spr_width, spr_width);
+	size = v2_sub(size, spr_size);
+
+	draw_image(
+		sprites[SPRITE_tile_selector_tl],
+		v2(pos.x, pos.y + size.y),
+		spr_size,
+		COLOR_WHITE
+	);
+	draw_image(
+		sprites[SPRITE_tile_selector_tr],
+		v2_add(pos, size),
+		spr_size,
+		COLOR_WHITE
+	);
+	draw_image(
+		sprites[SPRITE_tile_selector_bl],
+		pos,
+		spr_size,
+		COLOR_WHITE
+	);
+	draw_image(
+		sprites[SPRITE_tile_selector_br],
+		v2(pos.x + size.x, pos.y),
+		spr_size,
+		COLOR_WHITE
+	);
+}
+
+/* Entity Functions */
+
+Entity* entity_create() {
+	Entity* entity_found = NULL;
+
+	for (int i = 0; i < MAX_ENTITIES; i++) {
+		Entity* existing_entity = &world->entities[i];
+		if (!existing_entity->is_valid) {
+			entity_found = existing_entity;
+			break;
+		}
+	}
+
+	assert(entity_found, "No more free entities.");
+
+	return entity_found;
+}
+
+void entity_destroy(Entity* ent) {
+	memset(ent, 0, sizeof(Entity));
+}
+
+Range2f entity_get_rect(Entity* ent) {
+	return range2f_bottom_center(
+		ent->pos, v2_add(ent->pos, ent->size)
+	);
+}
+
+void setup_player(Entity* player) {
+	player->is_valid = true;
+	player->type = ENT_player;
+	player->tile_size = v2i(1, 1);
+	player->sprite = sprites[SPRITE_player];
+	player->color = COLOR_WHITE;
+	player->size
+		= v2(player->sprite->width, player->sprite->height);
+	player->pos = v2(0, 0);
+	player->rect = entity_get_rect(player);
+}
+
+void setup_entity(
+	Entity* ent,
+	EntityType type,
+	Gfx_Image* sprite
+) {
+	int width = WINDOW_WIDTH * SCALE / TILE_SIZE;
+	int height = WINDOW_HEIGHT * SCALE / TILE_SIZE;
+	int x = get_random_int_in_range(
+		width / -2, (width / 2) - ent->tile_size.x
+	);
+	int y = get_random_int_in_range(
+		height / -2, (height / 2) - ent->tile_size.y
+	);
+
+	ent->tile_pos = v2i(x, y);
+	ent->tile_size = v2i(2, 2);
+	ent->is_valid = true;
+	ent->type = type;
+	ent->sprite = sprite;
+	ent->color = COLOR_WHITE;
+	ent->size = v2(sprite->width, sprite->height);
+	ent->pos = v2_add(
+		tile_to_world_pos(ent->tile_pos),
+		v2_mul(v2(ent->tile_size.x / 2.0f, 0.33f), tile_sizef)
+	);
+	ent->rect = entity_get_rect(ent);
+}
+
+/* Load Functions */
+
+void load_sprites(Allocator allocator) {
+	sprites[SPRITE_player] = load_image_from_disk(
+		STR("assets/astronaut.png"), allocator
+	);
+	assert(
+		sprites[SPRITE_player], "Failed to load player sprite."
+	);
+	sprites[SPRITE_energy_crystal] = load_image_from_disk(
+		STR("assets/energy-crystal.png"), allocator
+	);
+	assert(
+		sprites[SPRITE_energy_crystal],
+		"Failed to load energy_crystal sprite."
+	);
+	sprites[SPRITE_crystal_drop] = load_image_from_disk(
+		STR("assets/crystal-drop.png"), allocator
+	);
+	assert(
+		sprites[SPRITE_crystal_drop],
+		"Failed to load crystal_drop sprite."
+	);
+	sprites[SPRITE_berry_stalk] = load_image_from_disk(
+		STR("assets/berry-stalk.png"), allocator
+	);
+	assert(
+		sprites[SPRITE_berry_stalk],
+		"Failed to load berry_stalk sprite."
+	);
+	sprites[SPRITE_berry_drop] = load_image_from_disk(
+		STR("assets/berry-drop.png"), allocator
+	);
+	assert(
+		sprites[SPRITE_berry_drop],
+		"Failed to load berry_drop sprite."
+	);
+	sprites[SPRITE_tile_selector_tl] = load_image_from_disk(
+		STR("assets/tile-selector-tl.png"), allocator
+	);
+	assert(
+		sprites[SPRITE_tile_selector_tl],
+		"Failed to load tile_selector_tl sprite."
+	);
+	sprites[SPRITE_tile_selector_bl] = load_image_from_disk(
+		STR("assets/tile-selector-bl.png"), allocator
+	);
+	assert(
+		sprites[SPRITE_tile_selector_bl],
+		"Failed to load tile_selector_bl sprite."
+	);
+	sprites[SPRITE_tile_selector_tr] = load_image_from_disk(
+		STR("assets/tile-selector-tr.png"), allocator
+	);
+	assert(
+		sprites[SPRITE_tile_selector_tr],
+		"Failed to load tile_selector_tr sprite."
+	);
+	sprites[SPRITE_tile_selector_br] = load_image_from_disk(
+		STR("assets/tile-selector-br.png"), allocator
+	);
+	assert(
+		sprites[SPRITE_tile_selector_br],
+		"Failed to load tile_selector_br sprite."
 	);
 }
