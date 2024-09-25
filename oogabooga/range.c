04a0aa2efbe2df0ca13621517fc4815c0414a6fb
benrbowers <benjambowers@gmail.com>
2024-09-24 21:01:22 -0400

    Declare simpler functions as inline

--- oogabooga/range.c
+++ oogabooga/range.c
@@ -34,27 +34,34 @@ inline Range2i range2i(Vector2i min, Vector2i max) {
 	return (Range2i){min, max};
 }
 
-Range1f range1f_shift(Range1f r, float shift) {
+Range2f pos_size_to_range2f(Vector2 pos, Vector2 size) {
+	return range2f(pos, v2_add(pos, size));
+}
+Range2i pos_size_to_range2i(Vector2i pos, Vector2i size) {
+	return range2i(pos, v2i_add(pos, size));
+}
+
+inline Range1f range1f_shift(Range1f r, float shift) {
 	r.min += shift;
 	r.max += shift;
 	return r;
 }
-float range1f_size(Range1f range) {
+inline float range1f_size(Range1f range) {
 	return range.max - range.min;
 }
-bool range1f_contains(Range1f range, float v) {
+inline bool range1f_contains(Range1f range, float v) {
 	return v >= range.min && v <= range.max;
 }
-bool range1f_empty(Range1f range) {
+inline bool range1f_empty(Range1f range) {
 	return range.min == range.max;
 }
 
-Range2f range2f_shift(Range2f r, Vector2 shift) {
+inline Range2f range2f_shift(Range2f r, Vector2 shift) {
 	r.min = v2_add(r.min, shift);
 	r.max = v2_add(r.max, shift);
 	return r;
 }
-Vector2 range2f_size(Range2f range) {
+inline Vector2 range2f_size(Range2f range) {
 	return v2_sub(range.max, range.min);
 }
 Range2f range2f_bottom_center(Vector2 min, Vector2 max) {
@@ -63,36 +70,36 @@ Range2f range2f_bottom_center(Vector2 min, Vector2 max) {
 	range = range2f_shift(range, v2(size.x * -0.5, 0.0));
 	return range;
 }
-bool range2f_contains(Range2f range, Vector2 v) {
+inline bool range2f_contains(Range2f range, Vector2 v) {
 	return v.x >= range.min.x && v.x <= range.max.x
 		&& v.y >= range.min.y && v.y <= range.max.y;
 }
-bool range2f_empty(Range2f range) {
+inline bool range2f_empty(Range2f range) {
 	return range.min.x == range.max.x
 		&& range.min.y == range.max.y;
 }
 
-Range1i range1i_shift(Range1i r, int shift) {
+inline Range1i range1i_shift(Range1i r, int shift) {
 	r.min += shift;
 	r.max += shift;
 	return r;
 }
-int range1i_size(Range1i range) {
+inline int range1i_size(Range1i range) {
 	return range.max - range.min;
 }
-bool range1i_contains(Range1i range, int v) {
+inline bool range1i_contains(Range1i range, int v) {
 	return v >= range.min && v <= range.max;
 }
-bool range1i_empty(Range1i range) {
+inline bool range1i_empty(Range1i range) {
 	return range.min == range.max;
 }
 
-Range2i range2i_shift(Range2i r, Vector2i shift) {
+inline Range2i range2i_shift(Range2i r, Vector2i shift) {
 	r.min = v2i_add(r.min, shift);
 	r.max = v2i_add(r.max, shift);
 	return r;
 }
-Vector2i range2i_size(Range2i range) {
+inline Vector2i range2i_size(Range2i range) {
 	return v2i_sub(range.max, range.min);
 }
 Range2i range2i_bottom_center(Vector2i min, Vector2i max) {
@@ -101,11 +108,11 @@ Range2i range2i_bottom_center(Vector2i min, Vector2i max) {
 	range = range2i_shift(range, v2i(size.x * -0.5, 0.0));
 	return range;
 }
-bool range2i_contains(Range2i range, Vector2 v) {
+inline bool range2i_contains(Range2i range, Vector2 v) {
 	return v.x >= range.min.x && v.x <= range.max.x
 		&& v.y >= range.min.y && v.y <= range.max.y;
 }
-bool range2i_empty(Range2i range) {
+inline bool range2i_empty(Range2i range) {
 	return range.min.x == range.max.x
 		&& range.min.y == range.max.y;
 }
